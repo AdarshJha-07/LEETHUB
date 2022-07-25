edec09f37c4df8616b605ff2ab99e2230a599d50
AdarshJha-07 <71597760+AdarshJha-07@users.noreply.github.com>
2022-07-25 20:04:40 +0530

    Time: 15 ms (44.51%), Space: 7.2 MB (29.92%) - LeetHub

--- 38-count-and-say/38-count-and-say.cpp
+++ 38-count-and-say/38-count-and-say.cpp
@@ -1,33 +1,14 @@
 class Solution {
-    int n;
-    string cntsy(int i,string s){
-        if(i==n){
-        
-            return s;
-        }
-        unordered_map<int,int>ump;
-        string res="";
-        if(i==3)cout<<s<<endl;
-      for(int j=0;j<(int)s.size();j++){
-        //  if(i==3)cout<<j<<endl;
-       int c=s[j]-'0';
-          int cnt=0;
-          while(s[j]-'0'==c){
-              cnt++;
-              j++;
-          }
-          j--;
-       
-          res+=to_string(cnt)+to_string(c);
-          
-      }
-
-
-        return cntsy(i+1,res);
-    }
 public:
-    string countAndSay(int n) {
-         this->n=n;  
-        return cntsy(1,"1");
+    string countAndSay(int n, string s = "1") {
+    if(n==1) return s;
+    int i=0, j, len = s.size();
+    string ans = "";
+    while(i<len) {
+        j=i;
+        while(i<len && s[i]==s[j]) i++; 
+        ans += to_string(i-j) + s[j];
     }
+    return countAndSay(n-1, ans);
+}
 };
