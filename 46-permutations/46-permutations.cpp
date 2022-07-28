ad8fa8833a8db73dc369556394aa67059757a1c1
AdarshJha-07 <71597760+AdarshJha-07@users.noreply.github.com>
2022-07-28 11:27:47 +0530

    Time: 3 ms (82.05%), Space: 8 MB (37.41%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,28 +1,26 @@
 class Solution {
     vector<vector<int>>ans;
-    void solve(vector<int>temp,vector<int>&visited, vector<int>& nums){
+    void solve(int idx,vector<int>&visited, vector<int>& nums){
      
-        if(temp.size()==nums.size()){
-            ans.push_back(temp);
+        if(idx>=nums.size()){
+           ans.push_back(nums);
             return;
         }
        
-        for( int k=0; k<nums.size(); k++ ){
-            if(!visited[k]){
-                visited[k]=1;
-            temp.push_back(nums[k]);
-            solve(temp, visited ,nums);
-            temp.pop_back();
-            visited[k]=0;
+        for( int k=idx; k<nums.size(); k++ ){
+           
+              swap(nums[idx],nums[k]);
+              solve(idx+1, visited ,nums);
+              swap(nums[idx],nums[k]);
             }
         }
-    }
+    
 public:
     vector<vector<int>> permute(vector<int>& nums) {
         
-                vector<int>visited(nums.size(),0);
+        vector<int>visited(nums.size(),0);
         vector<int>temp;
-        solve(temp,visited,nums);
+        solve(0,visited,nums);
         return ans;
     }
 };
