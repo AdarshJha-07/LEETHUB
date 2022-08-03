c34a6a4010e142116ee63960b4c40067dee838b1
AdarshJha-07 <71597760+AdarshJha-07@users.noreply.github.com>
2022-08-03 09:54:48 +0530

    Time: 176 ms (12.81%), Space: 64 MB (19.65%) - LeetHub

---
+++ 713-subarray-product-less-than-k/713-subarray-product-less-than-k.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& A, int k) {
+    
+        vector<int>pre(A.size(),1);
+      
+        // unordered_map<int,int>ump;
+        int sum=1;
+        int l = 0;
+        int cnt=0;
+        for(int i=0;i<A.size(); i++){
+             sum=sum*A[i];
+           if(sum>=k){
+               while(l<=i&&sum>=k){
+                   sum=sum/A[l++];
+              //l++;
+                   //su
+               }
+           }
+            cnt+=i-l+1;
+        }
+        return cnt;
+    }
+};
