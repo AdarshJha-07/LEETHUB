b5686a1df59d796ba11607331e323e550407d5e4
AdarshJha-07 <71597760+AdarshJha-07@users.noreply.github.com>
2022-08-04 11:10:03 +0530

    Time: 11 ms (26.16%), Space: 12.2 MB (36.55%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -6,12 +6,13 @@ public:
                 if(nums[i+1]>nums[i]){
                     int mx=i+1;
                     for(int j= i+1; j<nums.size(); j++){
-                        if(nums[mx]>nums[j]&&nums[j]>nums[i]){
+                        if(nums[mx]>=nums[j]&&nums[j]>nums[i]){
                          mx=j;   
                         }
                     }
                     swap(nums[mx],nums[i]);
-                    sort(nums.begin()+i+1,nums.end());
+                   reverse(nums.begin()+i+1, nums.end());
+                    
                     return;
                 }
                 
