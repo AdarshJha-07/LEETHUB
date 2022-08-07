358d65c67b20ad69e29642f336602660c97c04a0
AdarshJha-07 <71597760+AdarshJha-07@users.noreply.github.com>
2022-08-07 09:42:43 +0530

    Time: 1033 ms (7.90%), Space: 0B (100.00%) - LeetHub

--- 177-nth-highest-salary/177-nth-highest-salary.sql
+++ 177-nth-highest-salary/177-nth-highest-salary.sql
@@ -3,6 +3,6 @@ BEGIN
   RETURN (
       # Write your MySQL query statement below.
      
-     Select distinct e1.salary from Employee as e1 where N-1 = (Select Count(Distinct e2.salary) from Employee as e2 where e2.salary>e1.salary)
+     Select distinct(e1.salary) from Employee as e1 where N-1 = (Select Count(Distinct e2.salary) from Employee as e2 where e2.salary>e1.salary)
   );
 END
