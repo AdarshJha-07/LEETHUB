88b720e859547d29cb0b36312bdec9a760ec6e7c
AdarshJha-07 <71597760+AdarshJha-07@users.noreply.github.com>
2022-08-14 15:29:47 +0530

    Time: 2218 ms (23.30%), Space: 0B (100.00%) - LeetHub

---
+++ 607-sales-person/607-sales-person.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+
+# Select  sp.name  from Orders od join SalesPerson sp on od.sales_id= sp.sales_id join Company cp on od.com_id = cp.com_id where  cp.name <> 'RED';
+with cte as
+(Select sales_id from orders  o left join company c on o.com_id  = c.com_id where c.name ='RED')
+Select name from salesPerson  sp where sp.sales_id not  IN (select distinct cte.sales_id from cte);
