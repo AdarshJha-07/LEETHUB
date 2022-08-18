1b519f0b0fbf382735b6552b7822b809b945c10a
AdarshJha-07 <71597760+AdarshJha-07@users.noreply.github.com>
2022-08-19 01:28:53 +0530

    Time: 4 ms (60.85%), Space: 7.7 MB (87.02%) - LeetHub

---
+++ 996-number-of-squareful-arrays/996-number-of-squareful-arrays.cpp
@@ -0,0 +1,37 @@
+class Solution {
+    int ans=0;
+    int n;
+    int solve( int prev ,int mask ,vector<int>&nums){
+        
+        if(mask == (1<<n)-1)
+            return 1;
+        
+        int t = 0; 
+        for( int i = 0 ;i<nums.size(); i++){
+            
+            if(prev>=0 &&!isPerfect(nums[i] + nums[prev]))continue;
+            
+            if(i>0 && nums[i] == nums[i-1] &&!(mask&(1<<(i-1))))continue;
+            if(!(mask&(1<<i)) ){
+                
+             
+                    t += solve(i , mask|(1<<i), nums);                
+            }
+        }
+        
+        return t;
+    }
+    bool isPerfect(int val){
+        int z = sqrt(val);
+        
+        return z*z == val;
+    }
+    
+public:
+    int numSquarefulPerms(vector<int>& nums) {
+     
+        sort(nums.begin(),nums.end());
+        this -> n = nums.size();
+        return solve( -1 ,0, nums);
+    }
+};
