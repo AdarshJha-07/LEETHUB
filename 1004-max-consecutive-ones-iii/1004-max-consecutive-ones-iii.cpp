7a11d69a131f7a0eca369f5cdc54c257e59bf7ed
AdarshJha-07 <71597760+AdarshJha-07@users.noreply.github.com>
2022-09-06 09:49:03 +0530

    Time: 87 ms (65.66%), Space: 55.4 MB (65.22%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -1,48 +1,17 @@
 class Solution {
 public:
-    int longestOnes(vector<int>& nums, int k) {
+    int longestOnes(vector<int>& A, int K) {
         
-        int l = 0, r = 0;
-        int z  = k;
-        int ans = INT_MIN;
-        
-        unordered_map<int,int>ump;
-        
-        while( r<nums.size() )
-        {
-            if(nums[r]==1)
-            {
-                r++;
+        int zeroCount = 0, l = 0;
+        int maxLen = 0;
+        for( int r = 0 ; r < A.size() ; ++r ) {
+            if( A[ r ] == 0 ) ++zeroCount;
+            while( zeroCount > K ) {
+                if( A[ l ] == 0 ) --zeroCount;
+                ++l;
             }
-            else
-            {
-                if(z>0)
-                {
-                     z--;
-                     // ump[0].insert(r);
-                    ump[r]++;
-                     r++;
-                }
-                else{
-                    while(l<r && ump[l] == 0)
-                    {
-                        l++;
-                    }
-                   if(ump[l]) 
-                   {
-                      ump[l] = 0;
-                       l++;
-                       z++;
-                   }
-                    else l++,r++;
-                    
-                }
-                
-            }
-         
-            ans = max(ans , r-l);
+            maxLen = max( maxLen, r - l + 1 );
         }
-        
-        return ans;
+        return maxLen;
     }
 };
