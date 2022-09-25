4abb39745aa4fd0d4fe8385716dd98cd19648f29
AdarshJha-07 <71597760+AdarshJha-07@users.noreply.github.com>
2022-09-25 17:31:52 +0530

    Time: 577 ms (8.90%), Space: 84.4 MB (58.51%) - LeetHub

---
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.cpp
@@ -0,0 +1,38 @@
+class Solution {
+    bool check( int s ,long long k,vector<int>&arr){
+           
+        long long m = 0;
+        for( int i = arr.size()-1; i>=0; i--)
+        {
+            m += arr[i]/s;
+            if( m>= k) return true;
+        }
+        return false;
+    }
+public:
+    int maximumCandies(vector<int>& candies, long long k) {
+     
+    sort(candies.begin(),candies.end());
+        
+    long long ans = 0;
+      
+    int low = 1,high = *max_element(candies.begin(),candies.end()); 
+        int mid = (low + high)>>1;
+    while( low <= high)
+    {
+        //if(mid == 0) return 0;
+        if(check(mid ,k ,candies))
+        {
+            low = mid+1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+        mid = (low + high)>>1;
+    }
+     //cout<<low<<" "<<high<<endl;   
+    return high;
+        
+  }
+};
