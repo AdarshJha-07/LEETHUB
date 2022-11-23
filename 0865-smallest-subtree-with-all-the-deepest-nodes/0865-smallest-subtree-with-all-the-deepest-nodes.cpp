468659bd5f228be7f0245636b314164ade6f8ca4
AdarshJha-07 <71597760+AdarshJha-07@users.noreply.github.com>
2022-11-23 12:37:57 +0530

    Time: 10 ms (51.83%), Space: 14.2 MB (93.50%) - LeetHub

--- 0865-smallest-subtree-with-all-the-deepest-nodes/0865-smallest-subtree-with-all-the-deepest-nodes.cpp
+++ 0865-smallest-subtree-with-all-the-deepest-nodes/0865-smallest-subtree-with-all-the-deepest-nodes.cpp
@@ -10,62 +10,31 @@
  * };
  */
 class Solution {
-    // int ans;
-    // void height(TreeNode* root , int op){
-    //     if(root == NULL) return;
-    //     if(op > ans){
-    //         ans = op;
-    //     }
-    //     height(root->left, op+1);
-    //     height(root->right,op+1);
-    // }
-     TreeNode* lca(TreeNode* root, TreeNode* p, TreeNode* q) 
-     {
-        if(root == NULL) return NULL;
-        if(p == root) return root;
-        if(q == root) return root;
-        TreeNode * left = lca(root->left,p,q);
-        TreeNode * right = lca(root->right,p,q);
-        
-       if(left == NULL) return right;
-        if(right == NULL) return left;
-        
-        if(left != NULL && right != NULL) return root;
-        return NULL;
+   int ans;
+    void height(TreeNode* root , int op){
+        if(root == NULL) return;
+        if(op > ans){
+            ans = op;
+        }
+        height(root->left, op+1);
+        height(root->right,op+1);
     }
+    TreeNode* solve(TreeNode* root,int op){
+        if(root == NULL) return NULL;
+        
+        if(op==ans) return root;
+        TreeNode* left = solve(root->left,op+1);
+        TreeNode* right = solve(root->right,op+1);
+         
+      if(left && right) return root;
+     
+     return left == NULL?right : left;   
+    }
+    
 public:
     TreeNode* subtreeWithAllDeepest(TreeNode* root) {
-     
-        queue<TreeNode*>q;
-        q.push(root);
-        vector<TreeNode*>ans={root};
-        while(!q.empty())
-        {
-            int sz = q.size();
-            vector<TreeNode*>tem;
-            while(sz--){
-                TreeNode* temp = q.front();
-                q.pop();
-                if(temp->left != NULL)
-                {
-                    q.push(temp->left);
-                    tem.push_back(temp->left);
-                }
-                if(temp->right != NULL)
-                {
-                    q.push(temp->right);
-                    tem.push_back(temp->right);
-                }
-            }
-            if(tem.size()!=0)
-             ans = tem;
-        }
-       // cout<<ans.size()<<endl;
-        if(ans.size() == 1) return ans[0];
-        TreeNode* res = lca(root,ans[0],ans[1]);
-        for(int i = 2; i<ans.size(); i++){
-              res = lca(root,res,ans[i]);
-        }
-        return res;
+       ans = INT_MIN;
+       height(root,0);
+      return solve(root,0);
     }
 };
