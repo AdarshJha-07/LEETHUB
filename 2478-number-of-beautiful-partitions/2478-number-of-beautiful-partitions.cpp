b0c00accfe1f15cce82b5b45a68de38a82e34890
AdarshJha-07 <71597760+AdarshJha-07@users.noreply.github.com>
2022-11-25 12:47:45 +0530

    Time: 2977 ms (5.01%), Space: 68 MB (17.74%) - LeetHub

--- 2478-number-of-beautiful-partitions/2478-number-of-beautiful-partitions.cpp
+++ 2478-number-of-beautiful-partitions/2478-number-of-beautiful-partitions.cpp
@@ -1,24 +1,34 @@
-class Solution
-{
-public:
-int dp[1001][1001] = {};
-bool isPrime(char ch) {
-    return ch == '2' || ch == '3' || ch == '5' || ch == '7';
-}
-int dfs(int i, int k, int len, string &s) {
-    if (k == 0)
-        return i == s.size();
-    if (i + len * k > s.size() || !isPrime(s[i]))
-        return 0;
-    if (dp[i][k] == 0) {
-        dp[i][k] = 1;
-        for (int j = i + len - 1; j < s.size(); ++j)
-            if (!isPrime(s[j]))
-                dp[i][k] = (dp[i][k] + dfs(j + 1, k - 1, len, s)) % 1000000007;
+class Solution {
+    bool check(char c)
+    {
+        return c == '2'|| c == '3'|| c == '5'|| c=='7';
     }
-    return dp[i][k] - 1;
-}
-int beautifulPartitions(string s, int k, int minLength) {
-    return dfs(0, k, minLength, s);
-}
+    int minL;
+    int mod;
+    int solve(int i,int k,string& s,vector<vector<long long>>&dp){
+        if(i == s.size()){
+            if(k == 0) return 1;
+            return 0;
+        }
+        
+        if(i + k*minL> s.size() || !check(s[i]) || k<=0 ) return 0;
+        if(dp[i][k] != -1) return dp[i][k];
+        long long ans = 0;
+        for(int j = minL+i-1; j<s.size(); j++)
+        {
+              if(!check(s[j]))
+               ans += solve(j+1,k-1,s,dp)%mod;         
+              
+        }
+        
+        return dp[i][k] = ans%mod;
+    }
+public:
+    int beautifulPartitions(string s, int k, int minLength) {
+        minL = minLength;
+        mod = 1e9+7;
+        vector<vector<long long>>dp(s.size(),vector<long long>(k+1,-1));
+    
+        return solve(0,k,s,dp); 
+    }
 };
