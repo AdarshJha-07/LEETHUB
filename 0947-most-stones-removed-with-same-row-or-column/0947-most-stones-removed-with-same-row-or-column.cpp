e7a64609e891af70597e8251402c72847ec6478e
AdarshJha-07 <71597760+AdarshJha-07@users.noreply.github.com>
2022-11-25 10:11:10 +0530

    Time: 1229 ms (5.02%), Space: 19.6 MB (25.03%) - LeetHub

---
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -0,0 +1,47 @@
+class Solution {
+    vector<int>vis;
+    
+      int dfs(int i,vector<int>adj[]){
+         
+          
+          vis[i]=1;
+          int ans = 0;
+         for(auto it: adj[i])
+         {
+             if(!vis[it])
+                ans += 1 + dfs(it,adj);    
+         } 
+          return ans;
+     }
+public:
+    
+    int removeStones(vector<vector<int>>& stones) {
+        int n = stones.size();
+        vector<int>adj[n];
+        vis = vector<int>(n,0);
+        
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<n; j++){
+                if(stones[i][0] == stones[j][0])
+                {
+                    adj[i].push_back(j);
+                    adj[j].push_back(i);
+                }
+                
+                if(stones[i][1] == stones[j][1])
+                {
+                     adj[i].push_back(j);
+                     adj[j].push_back(i);
+                }                
+            }
+        }
+        
+     int ans = 0;
+     for(int i= 0; i<n; i++){
+         if(!vis[i]){
+             ans += dfs(i,adj);
+         }
+     } 
+       return ans;
+    }
+};
